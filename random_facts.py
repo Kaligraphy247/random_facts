5503021c5c0f1141cc4633cfee4ee5abb1f4dbbb
kaligraphy247 <chikeziejames3@gmail.com>
2022-07-18 11:59:37 +0200

    Added the feature to add send quotes as images

--- random_facts.py
+++ random_facts.py
@@ -22,24 +22,26 @@ port = 465 #use 587 tls, 465 is for ssl?
 
 ##############################
 random_facts = [
-    "Every journalist has a movel in him, which is an excellent place for it. - Ryssel Lynes",
+    "Every journalist has a movie in him, which is an excellent place for it. - Ryssel Lynes",
     "Just because someone doesn't react, doesn't mean they didn't notice.",
-    "I got kicked out of a ballet class because I pulled a groin muscle. it wasn't mine. - Rita Rudner",
+    "I got kicked out of a ballet class because I pulled a groin muscle. It wasn't mine. - Rita Rudner",
     "80% of all serious or fatal car craches are caused by men.",
     "Nothing is impossible. Some things are just more likely than others. - Jonathan Winters",
     "A person that truly loves you will let you go, no matter how hard the situation is.",
     "A psychiatrist is a fellow who asks you a lot of expensive questions your wife asks for nothing. - Joey Adams",
-    "Girls who have high trust with their fathers also have tend to have high trust with their boyfriends."
+    "Girls who have high trust with their fathers also have tend to have high trust with their boyfriends.",
+    "You've gotta dance like there's nobody watching. - William W. Purkey",
+    "When a man learns to love, he must bear the risk of hatred.",
+    "Books can also provoke emotions.And emotions sometimes are even more troublesome than ideas. Emotions have led people to do all sorts of things they later regret-like, oh, throwing a book at someone else.",
     ]
 
 
-def select_random_fact(random_fact: list):
+def select_random_fact(random_fact: list) -> str:
     return random.choice(random_fact)
 
 def send_fact_as_email():
     email = EmailMessage()
     email["From"] = sender
-    #email["To"] = receipients
     email['To'] = ', '.join(receipients)
     email['Subject'] = "Random Facts! - with Python"
     email.set_content(select_random_fact(random_facts))
